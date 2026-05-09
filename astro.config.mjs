f6a0395ebeef9ced077b4b35e457d76a03f61213
Phil Steer <pdincubus@users.noreply.github.com>
2026-05-09 14:20:56 +0100

    Add Astro MDX integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-    site: 'https://lscss.dev'
+    site: 'https://lscss.dev',
+
+    integrations: [
+        mdx()
+    ]
 });
