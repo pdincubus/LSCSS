c1feb585518d4a9d67bf29a70a44233954c05d9b
Phil Steer <pdincubus@users.noreply.github.com>
2026-05-09 16:43:15 +0100

    Load code example styles from Prism init

--- public/scripts/prism-init.js
+++ public/scripts/prism-init.js
@@ -5,4 +5,20 @@ import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-bash';
 
+function loadStylesheet(href) {
+    if (document.querySelector(`link[href="${href}"]`)) {
+        return;
+    }
+
+    const link = document.createElement('link');
+
+    link.rel = 'stylesheet';
+    link.href = href;
+
+    document.head.append(link);
+}
+
+loadStylesheet('/styles/components/code-example.css');
+loadStylesheet('/styles/vendor/prism.css');
+
 Prism.highlightAll();
