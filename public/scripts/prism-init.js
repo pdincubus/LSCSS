fd0e3dd9641a2c56305b645c72d6302481afb64e
Phil Steer <pdincubus@users.noreply.github.com>
2026-05-09 16:34:55 +0100

    Add Prism initialisation script

---
+++ public/scripts/prism-init.js
@@ -0,0 +1,8 @@
+import Prism from 'prismjs';
+
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-bash';
+
+Prism.highlightAll();
